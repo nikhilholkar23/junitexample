a6e5f49025fd92f139be61093652da5c88038897
Nikhil <nh@sysbitlabs.in>
2020-11-09 11:07:00 +0530

    new project added

---
+++ demo2/src/test/java/com/example/demo2/Demo2ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.demo2;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ demo2/src/test/java/com/example/demo2/TestWebApp.java
@@ -0,0 +1,34 @@
+package com.example.demo2;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+public class TestWebApp extends Demo2ApplicationTests {
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @BeforeAll
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void testEmployee() throws Exception {
+        mockMvc.perform(get("/employee")).andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andExpect(jsonPath("$.name").value("emp1")).andExpect(jsonPath("$.designation").value("manager"))
+                .andExpect(jsonPath("$.empId").value("1")).andExpect(jsonPath("$.salary").value(3000));
+
+    }
+
+}
